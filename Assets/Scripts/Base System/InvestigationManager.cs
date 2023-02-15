3f63aea04f85e4826ceec478405ed7b510a79ba5
jingyu-xie <jxie2@wpi.edu>
2023-02-15 11:54:26 -0500

    ClueBase

---
+++ Assets/Scripts/Base System/InvestigationManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InvestigationManager : Singleton<InvestigationManager>
+{
+    [SerializeField]
+    private GameObject ClueBase;
+    private GameObject newClue;
+    private GameObject tempClue;
+
+    public void AddCluePrefab(string clueName)
+    {
+        newClue = Resources.Load<GameObject>("CluePrefabs/" + clueName);
+        tempClue = (GameObject)Instantiate(newClue);
+        tempClue.GetComponent<Transform>().SetParent(ClueBase.GetComponent<Transform>(), true); 
+    }
+}
