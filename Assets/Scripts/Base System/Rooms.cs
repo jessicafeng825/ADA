f744b3ecadb8a1f19891d5b75cad0627bf383015
JamesAmigo <jijia66@gmail.com>
2023-03-13 16:19:23 -0400

    Room movement

--- Assets/Scripts/Base System/Rooms.cs
+++ Assets/Scripts/Base System/Rooms.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class Rooms : MonoBehaviour
 {
-    public enum Memory
-    {
-        Masnion,
-        Street,
-        NightClub,
-        LawyersOffice,
-        VoidBase
-    }
-    
-    //These are the properties of the rooms
-    [field: SerializeField]
-    public Memory locatedMemory 
-    { get; private set;}
 
     [field: SerializeField]
     public string roomName 
