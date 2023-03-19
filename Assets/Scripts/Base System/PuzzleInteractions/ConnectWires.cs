bda8cc6cc8862c969c3cc20cd4abd7d725038b5a
jingyu-xie <jxie2@wpi.edu>
2023-03-19 14:17:40 -0400

    PUSH

--- Assets/Scripts/Base System/PuzzleInteractions/ConnectWires.cs
+++ Assets/Scripts/Base System/PuzzleInteractions/ConnectWires.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ConnectWires : PuzzleInfo
 {
-    private Vector2[,] points = new Vector2[7,5];
+    [SerializeField]
+    private GameObject[] points;
+    [SerializeField]
+    private LineRenderer lineRenderer;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && OnInitialPoint())
         {
-
+            Debug.Log("start");
+            lineRenderer.SetPosition(0, points[0].transform.position);
+            lineRenderer.SetPosition(1, Input.mousePosition);
         }
+    }
+
+    private bool OnInitialPoint()
+    {
+        return Input.mousePosition == points[0].transform.position;
     }
 }
