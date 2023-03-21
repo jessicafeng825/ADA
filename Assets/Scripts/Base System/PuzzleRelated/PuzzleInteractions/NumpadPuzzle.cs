1b6d734c6a4175f23a2856a2e83d9684f51251e1
JamesAmigo <jijia66@gmail.com>
2023-03-21 15:53:08 -0400

    Numpad

--- Assets/Scripts/Base System/PuzzleRelated/PuzzleInteractions/NumpadPuzzle.cs
+++ Assets/Scripts/Base System/PuzzleRelated/PuzzleInteractions/NumpadPuzzle.cs
@@ -7,24 +7,47 @@ using UnityEngine.UI;
 public class NumpadPuzzle : PuzzleInfo
 {
     [SerializeField]
-    private TextMeshProUGUI answerText;
+    private GameObject answerText;
 
     [SerializeField]
     private string answer;
 
     private string enteredNum = "";
 
+    private void Start()
+    {
+        answerText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = new string('0', answer.Length);
+    }
     public void NumEnter(int num)
     {
         if(num == -1 && enteredNum.Length > 0)
         {
-            enteredNum.Remove(enteredNum.Length - 1);
+            enteredNum = enteredNum.Remove(enteredNum.Length - 1, 1);
+            if(enteredNum.Length == 0)
+            {
+                answerText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = new string('0', answer.Length);
+                Debug.Log("empty");
+                return;
+            }
+            answerText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = enteredNum;
         }
-        if(num >= 0 && num <= 9 && enteredNum.Length < answer.Length)
+        else if(num >= 0 && num <= 9 && enteredNum.Length < answer.Length)
         {
             enteredNum += num.ToString();
+            answerText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = enteredNum;
         }
-        answerText.text = enteredNum;
+    }
+    public void CheckAnswer()
+    {
+        if(enteredNum == answer)
+        {
+            Debug.Log("correct");
+            isSolved = true;
+        }
+        else
+        {
+            Debug.Log("incorrect");
+        }
     }
     
 }
