924c53d2d8adec86f48bcda44edd2fdf238d3e9b
jingyu-xie <jxie2@wpi.edu>
2023-04-06 13:25:58 -0400

    intro view other players

---
+++ Assets/Scripts/CharacterInfoSimple.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfoSimple : MonoBehaviour
+{
+    public string playerImage;
+    public string jobName;
+    public string characterName;
+
+    [TextArea(5, 20)]
+    public string brief;
+}
